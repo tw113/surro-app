9526c2bbb308e92a987b57b2e962b9669598fe0d
wij2012 <joh15132@byui.edu>
2018-03-02 20:21:46 -0700

    got some of the basic functions including the constructor for the profile class mostly set

--- app/src/main/java/ussurrogacy/com/surrogateapp/Profile.java
+++ app/src/main/java/ussurrogacy/com/surrogateapp/Profile.java
@@ -18,13 +18,45 @@ public class Profile {
     private String status;
     private float bmi;
 
-    public Profile (List<String> keys, List<String> values, int num){
+    public Profile (List<String> questions, List<String> answers, int num){
 
-    };
+        //set the values of data using the questions as the key and answers as the values
+        for (int i = 0; i < questions.size(); i++)
+        {
+            data.put((questions.get(i)), answers.get(i));
+        }
 
-    /*
+        //set the id of the profile
+        id = num;
+
+        //set the status as created. Will later be changed to
+        //approved or denied
+        status = "Profile created.";
+
+        //set the bmi
+    }
+
+    //function to change status of the profile
+    public void changeStatus(String newStatus)
+    {
+        status = newStatus;
+    }
+
+    //function to return the status of the profile
+    public String getStatus()
+    {
+        return status;
+    }
+
+    //function to return profile id number
+    public int getId()
+    {
+        return id;
+    }
+
+    //function to get the bmi of the profile
     public float getBmi() {return bmi;}
 
-    public void setBmi() {bmi = (weight * 703)/(height * height);}
-    */
+    //public void setBmi() {bmi = (weight * 703)/(height * height);}
+
 }
