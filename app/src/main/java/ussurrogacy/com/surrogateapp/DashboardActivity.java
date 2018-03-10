f9b9d290fe9203630e2157baf6b04d0924ffd072
wij2012 <joh15132@byui.edu>
2018-03-10 11:32:18 -0700

    started on the fragment for list view and added getdata in the profile class

--- app/src/main/java/ussurrogacy/com/surrogateapp/DashboardActivity.java
+++ app/src/main/java/ussurrogacy/com/surrogateapp/DashboardActivity.java
@@ -1,10 +1,15 @@
 package ussurrogacy.com.surrogateapp;
 
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -27,6 +32,7 @@ public class DashboardActivity extends AppCompatActivity {
 
     private GoogleAccountCredential mCredential;
     private ProgressBar mProgress;
+    private static List<Profile> profiles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -114,7 +120,7 @@ public class DashboardActivity extends AppCompatActivity {
             List<String> questions = new ArrayList<String>();
             List<String> answers = new ArrayList<String>();
             Map<String, String> map = new LinkedHashMap<>();
-            List<Profile> profiles = new ArrayList<>();
+            profiles = new ArrayList<>();
 
             if (values != null) {
                 for (int i = 0; i < values.size(); i++) {
@@ -154,3 +160,95 @@ public class DashboardActivity extends AppCompatActivity {
         }
     }
 }
+
+//the listview fragment class of the dashboard activity
+
+public class ListView extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_activity);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        //List<Profile> profiles =
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+}
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private String[] mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTextView;
+        public ViewHolder(TextView v) {
+            super(v);
+            mTextView = v;
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MyAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        // create a new view
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.my_text_view, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.mTextView.setText(mDataset[position]);
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
+
+
+/*
+*
+*
+*
+*
+* Need an adapter for the listview recycler to show
+* the items in the view and that will delete items no
+* longer available and add new items as new items are passed in
+*
+*
+*
+*
+ */
